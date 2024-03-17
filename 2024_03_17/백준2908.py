e6ee19ef27a2f99d1e1655a1f2f535967d3a8958
bled214 <57388197+bled214@users.noreply.github.com>
2024-03-17 13:40:56 +0900

    Update 백준2908.py

--- 2024_03_17/백준2908.py
+++ 2024_03_17/백준2908.py
@@ -13,6 +13,16 @@ number2 = tmp_list[1]#893
 number1 = number1[::-1]
 number2 = number2[::-1]
 
+# 문자열 뒤집기 - for문
+tmp1 = ''
+tmp2 = ''
+for i in range(len(number1)-1, -1, -1):
+	tmp1 = number1[i]
+for i in range(len(number2)-1, -1, -1):
+	tmp2 = number2[i]
+number1 = tmp1
+number2 = tmp2
+
 # 문자열 => 정수로 변경
 number1 = int(number1)
 number2 = int(number2)
